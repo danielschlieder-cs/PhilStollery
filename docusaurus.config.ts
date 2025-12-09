1963d253ea919287c9345e013ea292b89a3b1b07
dasc <info@schlieder.de>
2025-12-09 08:13:39 +0100

    Added required config changes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://philstollery.github.io',
+  url: 'https://github.com/danielschlieder-cs',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/github-usaurus/',
+  baseUrl: '/PhilStollery/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'philstollery', // Usually your GitHub org/user name.
-  projectName: 'github-usaurus', // Usually your repo name.
+  organizationName: 'comstack', // Usually your GitHub org/user name.
+  projectName: 'PhilStollery', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
